962826a2d99bc0c1388fa7ebf2006c720cd0d793
ekaru04 <kumagawaekaru04@gmail.com>
2021-07-21 17:18:25 +0700

    Update

--- dijkstra/RunTest.php
+++ dijkstra/RunTest.php
@@ -36,50 +36,50 @@ function runTest($input_a,$input_b) {
 
 }
 
-function runTestold($input_a,$input_b) {
-	$g = new Graph();
-	$g->addedge("a", "b", 4);
-	$g->addedge("a", "d", 1);
+// function runTestold($input_a,$input_b) {
+// 	$g = new Graph();
+// 	$g->addedge("a", "b", 4);
+// 	$g->addedge("a", "d", 1);
 
-	$g->addedge("b", "a", 74);
-	$g->addedge("b", "c", 2);
-	$g->addedge("b", "e", 12);
+// 	$g->addedge("b", "a", 74);
+// 	$g->addedge("b", "c", 2);
+// 	$g->addedge("b", "e", 12);
 
-	$g->addedge("c", "b", 12);
-	$g->addedge("c", "j", 12);
-	$g->addedge("c", "f", 74);
+// 	$g->addedge("c", "b", 12);
+// 	$g->addedge("c", "j", 12);
+// 	$g->addedge("c", "f", 74);
 
-	$g->addedge("d", "g", 22);
-	$g->addedge("d", "e", 32);
+// 	$g->addedge("d", "g", 22);
+// 	$g->addedge("d", "e", 32);
 
-	$g->addedge("e", "h", 33);
-	$g->addedge("e", "d", 66);
-	$g->addedge("e", "f", 76);
+// 	$g->addedge("e", "h", 33);
+// 	$g->addedge("e", "d", 66);
+// 	$g->addedge("e", "f", 76);
 
-	$g->addedge("f", "j", 21);
-	$g->addedge("f", "i", 11);
+// 	$g->addedge("f", "j", 21);
+// 	$g->addedge("f", "i", 11);
 
-	$g->addedge("g", "c", 12);
-	$g->addedge("g", "h", 10);
+// 	$g->addedge("g", "c", 12);
+// 	$g->addedge("g", "h", 10);
 
-	$g->addedge("h", "g", 2);
-	$g->addedge("h", "i", 72);
+// 	$g->addedge("h", "g", 2);
+// 	$g->addedge("h", "i", 72);
 
-	$g->addedge("i", "j", 7);
-	$g->addedge("i", "f", 31);
-	$g->addedge("i", "h", 18);
+// 	$g->addedge("i", "j", 7);
+// 	$g->addedge("i", "f", 31);
+// 	$g->addedge("i", "h", 18);
 
-	$g->addedge("j", "f", 8);
+// 	$g->addedge("j", "f", 8);
 
 
-	list($distances, $prev) = $g->paths_from($input_a);
+// 	list($distances, $prev) = $g->paths_from($input_a);
 
-	$path = $g->paths_to($prev, $input_b);
+// 	$path = $g->paths_to($prev, $input_b);
 
-	//print_r($path);
-	return $path;
+// 	//print_r($path);
+// 	return $path;
 
-}
+// }
 
 
 //runTest();
--- kecamatan.php
+++ kecamatan.php
@@ -6,7 +6,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Rute Terpendek Greedy</title>
+  <title>Rute Terpendek Kabupaten Bondowoso</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Font Awesome -->
@@ -48,23 +48,15 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-           style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+    <a href="./ruteTerpendek.php" class="brand-link">
+      <img src="dist/img/loc.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8;" style="height: 10px;">
+      <span class="brand-text font-weight-light">Rute Terpendek</span>
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-          <a href="#" class="d-block">TES</a>
-        </div>
-      </div>
+      
 
       <!-- Sidebar Menu -->
       <nav class="mt-2">
